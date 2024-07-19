2ffc52b9e3e5b1bdd552c47c4459f849785a9c1f
Arushi Sharma <sharmaarushi424@gmail.com>
2024-07-19 23:59:05 +0530

    git-ignore modified

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-const heading = React.createElement(
-    "h1",
-    {id:"heading", xyz:"abc"},
-     "Hello World!"
-    );
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h2",{},"I am an h1 tag"),
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h2",{},"I am an h1 tag"),
+    ]),
+]);
+console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
